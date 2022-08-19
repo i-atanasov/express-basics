943d4ba851bc8e69d225e63aa9cc929de4ea9c9e
i-atanasov <ivayloatanasov@zohomail.eu>
2022-08-19 13:46:12 +0300

    delete method

--- app.js
+++ app.js
@@ -5,19 +5,18 @@ const logReq = require('./middlewareLogReq')
 const PORT = 5000;
 const app = express();
 
-app.use('/api', logReq);
+app.use('/', logReq);
 
-//form for the post request to do
+//// parse form data, json
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static('./addData'));
+app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send("homepage");
-})
+//homepage
+app.use(express.static('./addData'));
 
 app.get('/api/movies', (req, res) => {
     const fetchedMovies = movies.map((movie) => {
-    const { id, title, watched } = movie
+        const { id, title, watched } = movie
         return { id, title, watched }
     })
 
@@ -26,7 +25,6 @@ app.get('/api/movies', (req, res) => {
 
 app.get('/api/movie/:id', (req, res) => {
     const movieID = req.params.id;
-    
     const singleMovie = movies.find(
         movie => movie.id === Number(movieID)
     )
@@ -37,20 +35,42 @@ app.get('/api/movie/:id', (req, res) => {
     return res.json(singleMovie)
 })
 
-app.post('/newMovie', (req, res) => {
+app.post('/api/movie/add', (req, res) => {
     let title = ''
     if (req.body.title) { title = req.body.title }
-    else { return res.status(400).send("<h3>Please provide title.</h3></bg><a href='/'>Back</a>") }
+    else { return res.status(400).send("<h3>Please provide a title.</h3></bg><a href='/'>Back</a>") }
     let watched = req.body.watched ? true : false
     const newMovies = [...movies, { id: movies[movies.length-1].id + 1, title: title, watched: watched}]
-    res.status(201).send("<h3>added successfully</h3></bg><a href='/'>Back</a>")
     console.log(newMovies)
+    let output = ''
+    for (i=0; i<newMovies.length; i++){
+        output += `${newMovies[i].title} <br />`
+        }
+
+    res.status(201).send(
+        `<h3>${title} added successfully</h3><a href='/'>Back</a>. </br>All titles:</br> ${output}`)
+   
 })
 
-//delete req to do
+//only works in Postman?
+app.delete('/deleteMovie/:id', (req, res) => {
+    const deletedMovieID = req.params.id;
+    const newMovies = movies.filter(
+        movie => movie.id !== Number(deletedMovieID)
+        )
+    console.log(newMovies)
+    
+    if (movies.length === newMovies.length) {
+        return res
+                .status(404)
+                .json({ success: false, msg: `no movie with id ${deletedMovieID}` })
+    }
+    return res.status(201).json(`<h3>Successfully deleted movie N:${deletedMovieID}.</h3>`)
+})
 
-//other
+//wrong requests
 app.all('*', (req, res) => {
+
     res.status(404).send('<h3>Resourse not found</h3>')
 })
 
